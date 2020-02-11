a67df26342fd13c925455a142c1a0688738ef14d
Hannes Kruse <Hannes.Kruse@dlr.de>
2020-02-11 15:30:20 +0100

    :recycle: Change structure of point-tuple.ts

--- src/loader/vtk-file-loader.ts
+++ src/loader/vtk-file-loader.ts
@@ -1,7 +1,7 @@
 // @ts-ignore
 import * as vtk from 'vtk.js';
-import PersistencePointTuple from '../point-tuple';
-import { ILoader, ILoaderData } from './loader-interface';
+import PersistencePointTuple, {Coordinate, CriticalType, Point3D} from '../point-tuple';
+import {ILoader, ILoaderData} from './loader-interface';
 
 export default class VtkFileLoader implements ILoader {
   /**
@@ -11,6 +11,10 @@ export default class VtkFileLoader implements ILoader {
   private readonly settings: {};
 
   private readonly reader: vtk.IO.Core.vtkHttpDataSetReader;
+
+  private rawPointData: Float32Array | undefined;
+  private criticalTypeData: Float32Array | undefined;
+  private coordinateData: Float32Array | undefined;
 
   /**
    * @param settings {{}} vtkHttpDataSetReader settings
@@ -41,22 +45,22 @@ export default class VtkFileLoader implements ILoader {
       this.reader.setUrl(fileName).then((reader: vtk.Reader) => {
         reader.loadData().then(() => {
           const rawData = reader.getOutputData().getPoints().getData();
+          const pointData = reader.getOutputData().getPointData();
 
           if (rawData.length % 3 !== 0) {
             throw new Error('Number of points not dividable by 3.');
           }
 
-          const points = [];
-          for (let i = 0; i < rawData.length; i += 6) {
-            points.push(new PersistencePointTuple(
-              rawData[i],
-              rawData[i + 1],
-              rawData[i + 2],
+          this.rawPointData = rawData;
+          this.criticalTypeData = pointData.getArray(1).getData();
+          this.coordinateData =  pointData.getArray(2).getData();
 
-              rawData[i + 3],
-              rawData[i + 4],
-              rawData[i + 5],
-            ));
+          const points = [];
+
+          let criticalTypeIndex = 0;
+          for (let i = 0; i < rawData.length; i += 6) {
+            points.push(this.createPointTuple(i, criticalTypeIndex));
+            criticalTypeIndex += 2;
           }
 
           console.info(`VKT data for file ${fileName} loaded.`);
@@ -70,12 +74,57 @@ export default class VtkFileLoader implements ILoader {
               return acc;
             }, { min: 0, max: 0 }),
           });
-        }).catch(() => {
-          reject();
+        }).catch((r:string) => {
+          reject(`Loader Error: ${r}.`);
         });
       }).catch(() => {
-        reject();
+        reject(`Could not access data at ${fileName}.`);
       });
     });
+  }
+
+  private createPointTuple(index: number, criticalIndex: number): PersistencePointTuple {
+    if (typeof this.rawPointData === 'undefined' ||
+        typeof this.coordinateData === 'undefined' ||
+        typeof this.criticalTypeData === 'undefined') {
+      throw new Error('Can\'t create PersistencePointTuple from undefined data.');
+    }
+
+    const lower: Point3D = {
+      x: this.rawPointData[index],
+      y: this.rawPointData[index + 1],
+      z: this.rawPointData[index + 2],
+    };
+
+    const upper: Point3D = {
+      x: this.rawPointData[index + 3],
+      y: this.rawPointData[index + 4],
+      z: this.rawPointData[index + 5],
+    };
+
+    const criticalType: CriticalType = {
+      lower: this.criticalTypeData[criticalIndex],
+      upper: this.criticalTypeData[criticalIndex + 1]
+    };
+
+    const coordinates: Coordinate = {
+      lower: {
+        x: this.coordinateData[index],
+        y: this.coordinateData[index + 1],
+        z: this.coordinateData[index + 2],
+      },
+      upper: {
+        x: this.coordinateData[index + 3],
+        y: this.coordinateData[index + 4],
+        z: this.coordinateData[index + 5],
+      }
+    };
+
+    return new PersistencePointTuple(
+        lower,
+        upper,
+        criticalType,
+        coordinates
+    );
   }
 }
--- src/point-tuple.ts
+++ src/point-tuple.ts
@@ -1,62 +1,52 @@
+export interface Point3D {
+  readonly x: number;
+  readonly y: number;
+  readonly z: number;
+}
+
+export interface Coordinate {
+  readonly lower: Point3D;
+  readonly upper: Point3D;
+}
+
+export interface CriticalType {
+  readonly lower: number;
+  readonly upper: number;
+}
+
 export default class PersistencePointTuple {
   /**
    * @type {number}
    */
   readonly persistence: number;
 
-  /*
-   * Lower Point Position
-   */
-  /**
-   * @type {number}
-   */
-  readonly x1: number;
+  readonly criticalType: CriticalType;
+
+  readonly coordinates: Coordinate;
 
   /**
-   * @type {number}
+   * Lower Point on canvas
    */
-  readonly y1: number;
+  readonly lower: Point3D;
 
   /**
-   * @type {number}
+   * Upper Point on canvas
    */
-  readonly z1: number;
-
-  /*
-   * Upper Point Position
-   */
-  /**
-   * @type {number}
-   */
-  readonly x2: number;
+  readonly upper: Point3D;
 
   /**
-   * @type {number}
+   * @param lower
+   * @param upper
+   * @param criticalType
+   * @param coordinates
    */
-  readonly y2: number;
+  constructor(lower: Point3D, upper: Point3D, criticalType: CriticalType, coordinates: Coordinate) {
+    this.lower = lower;
+    this.upper = upper;
+    this.criticalType = criticalType;
 
-  /**
-   * @type {number}
-   */
-  readonly z2: number;
+    this.coordinates = coordinates;
 
-  /**
-   * @param x1 {number} x-Position of Point 1
-   * @param y1 {number} y-Position of Point 1
-   * @param z1 {number} z-Position of Point 1
-   * @param x2 {number} x-Position of Point 2
-   * @param y2 {number} y-Position of Point 2
-   * @param z2 {number} z-Position of Point 2
-   */
-  constructor(x1: number, y1: number, z1: number, x2: number, y2: number, z2: number) {
-    this.x1 = x1;
-    this.y1 = y1;
-    this.z1 = z1;
-
-    this.x2 = x2;
-    this.y2 = y2;
-    this.z2 = z2;
-
-    this.persistence = y2 - y1;
+    this.persistence = upper.y - lower.y;
   }
 }
